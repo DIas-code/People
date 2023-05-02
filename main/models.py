399cf7875e844fe2ca7c3019d0abae0a691daabe
Dias <diasermekov09@gmail.com>
2023-05-02 22:18:51 +0600

    'gitignore

--- main/models.py
+++ main/models.py
@@ -18,11 +18,21 @@ class Place(models.Model):
 
     def __str__(self):
         return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{Category.name}'
 
 class Events(models.Model):
     name = models.CharField(max_length=120)
     date = models.DateTimeField()
     Place = models.ForeignKey(to=Place, on_delete=models.CASCADE)
+    Category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Event'
@@ -31,12 +41,23 @@ class Events(models.Model):
     def __str__(self):
         return f'Event {self.name} in {self.Place.name}'
 
+class Excursion(models.Model):
+    name = models.CharField(max_length=120)
+    num_of_people_now = models.PositiveIntegerField(default=0)
+    max_num_of_people = models.PositiveIntegerField()
+    date = models.DateTimeField()
+    Place = models.ForeignKey(to=Place, on_delete=models.CASCADE)
+    Category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Excursion in {self.Place.name}'
+
 class Guide(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     phone = models.CharField(max_length=120)
     image = models.ImageField(upload_to='guide_images', null=True, blank=True)
-    Area = models.ForeignKey(to=Area, on_delete=models.CASCADE)
+    Excursion = models.ForeignKey(to=Excursion, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Guide'
@@ -45,13 +66,6 @@ class Guide(models.Model):
     def __str__(self):
         return f'Guide {self.first_name} {self.last_name}'
 
-class Excursion(models.Model):
-    name = models.CharField(max_length=120)
-    num_of_people_now = models.PositiveIntegerField(default=0)
-    max_num_of_people = models.PositiveIntegerField()
-    date = models.DateTimeField()
-    Guide = models.ForeignKey(to=Guide, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'Excursion in {self.Guide.Area.location}'
-
+class Hotels(models.Model):
+    ...
+    Category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
